dd1350e61749c4ad80727c4241d627ca2e266ffb
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-07-26 09:51:33 +0200

    welcomecomponent

--- src/components/HomePage/Bienvenue.js
+++ src/components/HomePage/Bienvenue.js
@@ -1,30 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import videoBienv from "../../assets/videos/video1.mp4";
 import styles from "./Bienvenue.module.scss"; // Assurez-vous d'avoir le bon chemin d'importation
+
+const translations = {
+  fr: "Bienvenue",
+  en: "Welcome",
+  es: "Bienvenido",
+  de: "Willkommen",
+  // Add as many languages as needed
+};
+
+const WelcomeComponent = () => {
+  const [currentLanguage, setCurrentLanguage] = useState('fr');
+
+  useEffect(() => {
+    const intervalId = setInterval(changeLanguage, 1000);
+    return () => clearInterval(intervalId);
+  });
+
+  const changeLanguage = () => {
+    const languages = Object.keys(translations);
+    const nextIndex = (languages.indexOf(currentLanguage) + 1) % languages.length;
+    const nextLanguage = languages[nextIndex];
+    setCurrentLanguage(nextLanguage);
+  };
+
+  return (
+    <div id="welcome">
+      <span id="welcome-text" >
+        {translations[currentLanguage]}
+      </span>
+    </div>
+  );
+};
+
 function Bienvenue() {
   return (
     <>
-    <div className={styles.bienvenue}>
-    <div className={styles.backVideo}>
-    <div className={styles.content}>
-          <h2>Bienvenue</h2>
-          <p className={styles.comment}>"Harmonisez vos passions, partagez vos émotions avec Connectify !"</p>
+      <div className={styles.bienvenue}>
+        <div className={styles.backVideo}>
+          <div className={styles.content}>
+            <WelcomeComponent />
+            <p className={styles.comment}>"Harmonisez vos passions, partagez vos émotions avec Connectify !"</p>
+          </div>
+          <video
+            playsInline
+            autoPlay
+            muted
+            loop
+            
+          >
+            <source src={videoBienv} type="video/mp4" />
+            {/* Ajoutez des sources supplémentaires pour d'autres formats vidéo, si nécessaire */}
+          </video>
         </div>
-      <video
-        playsInline
-        autoPlay
-        muted
-        loop
-        style={{ width: "100%", height: "100%", objectFit: "cover" }}
-      >
-        <source src={videoBienv} type="video/mp4" />
-        {/* Ajoutez des sources supplémentaires pour d'autres formats vidéo, si nécessaire */}
-      </video>
-    </div>
-    
-    </div>
-
+      </div>
     </>
-  )
+  );
 }
+
 export default Bienvenue;
+

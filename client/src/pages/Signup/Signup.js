a69f13cf868f8fb5c80abccf7b8cb5d94918664c
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-07-27 16:30:11 +0200

    preparation api

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -2,8 +2,76 @@ import React from "react";
 import styles from "./Signup.module.scss";
 import imgIns from "../../assets/images/inscription.jpg";
 import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { createUser } from '../../apis/users';
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
+  const navigate = useNavigate();
+
+  const validationSchema = yup.object({
+    name: yup
+      .string()
+      .required("Il faut préciser votre nom")
+      .min(2, "Un vrai nom"),
+    lName: yup
+      .string()
+      .required("Il faut préciser votre nom")
+      .min(2, "Un vrai nom"),
+    email: yup
+      .string()
+      .required("Il faut préciser votre email")
+      .email("L'email n'est pas valide"),
+    imgProfile: yup
+      .string()
+      .required("Il faut renseigner une image")
+      .url("L'image doit être un lien valide"),
+    gender: yup.string().required("Il faut préciser votre genre"),
+    password: yup
+      .string()
+      .required("Il faut préciser votre mot de passe")
+      .min(6, "Mot de passe trop court"),
+    confirmPassword: yup
+      .string()
+      .required("Vous devez confirmer votre mot de passe")
+      .oneOf(
+        [yup.ref("password"), ""],
+        "Les mots de passe ne correspondent pas"
+      ),
+  });
+
+  const initialValues = {
+    name: "",
+    lName: "",
+    email: "",
+    imgProfile: "",
+    gender: "man",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    setError,
+    clearErrors,
+  } = useForm({
+    initialValues,
+    resolver: yupResolver(validationSchema),
+  });
+  const submit = handleSubmit(async (user) => {
+    try {
+      clearErrors();
+      await createUser(user);
+      navigate("/signin");
+    } catch (message) {
+      setError("generic", { type: "generic", message });
+    }
+  });
+
   return (
     <div className={`${styles.signup} d-flex justify-content-center`}>
       <div className={styles.image}>
@@ -14,7 +82,7 @@ function Signup() {
           <h2>Inscription</h2>
         </div>
         <div className={`${styles.content} d-flex `}>
-          <form action="" className={`${styles.form} p-20`}>
+          <form onSubmit={submit} className={`${styles.form} p-20`}>
             <div className="mb-10 d-flex flex-row justify-content-between">
               <div className="mb-10 d-flex flex-column ">
                 <label htmlFor="name" className={`${styles.input_1} mb-10`}>
@@ -25,7 +93,11 @@ function Signup() {
                   type="text"
                   name="name"
                   placeholder="Entrez votre nom..."
+                  {...register("name")}
                 />
+                {errors.name && (
+                  <p className="form-error">{errors.name.message}</p>
+                )}
               </div>
               <div className="mb-10 d-flex flex-column">
                 <label htmlFor="lName" className={`${styles.input_1} mb-10`}>
@@ -36,7 +108,11 @@ function Signup() {
                   type="text"
                   name="lName"
                   placeholder="Entrez votre prénom..."
+                  {...register("lName")}
                 />
+                {errors.lName && (
+                  <p className="form-error">{errors.lName.message}</p>
+                )}
               </div>
             </div>
             <div className="mb-10 d-flex flex-column">
@@ -48,7 +124,11 @@ function Signup() {
                 type="text"
                 name="email"
                 placeholder="Entrez votre mail..."
+                {...register("email")}
               />
+              {errors.email && (
+                <p className="form-error">{errors.email.message}</p>
+              )}
             </div>
             <div className="mb-10 d-flex flex-column">
               <label htmlFor="imgProfile" className="mb-10">
@@ -59,7 +139,11 @@ function Signup() {
                 type="text"
                 name="imgProfile"
                 placeholder="Entrez l'url pour votre image..."
+                {...register("imgProfile")}
               />
+              {errors.imgProfile && (
+                <p className="form-error">{errors.imgProfile.message}</p>
+              )}
             </div>
             <div className={`${styles.gender} d-flex py-20 `}>
               <label htmlFor="gender">Genre :</label>
@@ -68,12 +152,13 @@ function Signup() {
                   <input
                     className={`${styles.input_3} mr-15 ml-20`}
                     type="radio"
-                    value="male"
-                    id="male"
+                    value="man"
+                    id="man"
+                    {...register("gender")}
                   />
                 </div>
                 <div>
-                  <label htmlFor="male" className="mr-20 ">
+                  <label htmlFor="man" className="mr-20 ">
                     Homme
                   </label>
                 </div>
@@ -83,8 +168,9 @@ function Signup() {
                   <input
                     className={`${styles.input_3} mr-15`}
                     type="radio"
-                    value="female"
-                    id="female"
+                    value="woman"
+                    id="woman"
+                    {...register("gender")}
                   />
                 </div>
                 <div>
@@ -94,7 +180,11 @@ function Signup() {
                 </div>
               </div>
             </div>
-
+            <div className="mb-10">
+              {errors.gender && (
+                <p className="form-error">{errors.gender.message}</p>
+              )}
+            </div>
             <div className="mb-10 d-flex flex-column">
               <label htmlFor="password">Mot de passe :</label>
               <input
@@ -103,7 +193,11 @@ function Signup() {
                 type="password"
                 name="password"
                 placeholder="Entrez votre mot de passe..."
+                {...register("password")}
               />
+              {errors.password && (
+                <p className="form-error">{errors.password.message}</p>
+              )}
             </div>
             <div className="mb-10 d-flex flex-column">
               <label htmlFor="confirmPassword" className="mb-10">
@@ -115,10 +209,19 @@ function Signup() {
                 type="password"
                 name="confirmPassword"
                 placeholder="Entrez votre mot de passe..."
+                {...register("confirmPassword")}
               />
+              {errors.confirmPassword && (
+                <p className="form-error">{errors.confirmPassword.message}</p>
+              )}
             </div>
             <div className="d-flex justify-content-center ">
-              <button className="btn btn-secondary m-20">Valider</button>
+              <button
+                disabled={isSubmitting}
+                className="btn btn-secondary m-20"
+              >
+                Valider
+              </button>
             </div>
           </form>
         </div>

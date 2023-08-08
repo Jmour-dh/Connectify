c95b9197a3dac4496ca45a7b12c28f98607e1f7c
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-08-08 15:13:12 +0200

    change imgprofile

--- client/src/pages/Profile/components/Info/Info.js
+++ client/src/pages/Profile/components/Info/Info.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styles from "./Info.module.scss";
-import { useContext } from "react";
 import { AuthContext } from "../../../../context";
 
 function Info() {
   const { user } = useContext(AuthContext);
+  const [currentImgProfile, setCurrentImgProfile] = useState(user.imgProfile);
+
+  const handleImgProfileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    
+    if (selectedFile) {
+      const reader = new FileReader();
+      
+      reader.onload = (e) => {
+        setCurrentImgProfile(e.target.result);
+      };
+
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
   return (
     <div
       className={`${styles.imgBack} d-flex justify-content-center align-items-center`}
@@ -14,11 +29,18 @@ function Info() {
       >
         <ul className={styles.list}>
           <li>
-            <img
-              className={styles.imgProfile}
-              src={user.imgProfile}
-              alt="imgProfile"
-            />
+            <label>
+              <input
+                type="file"
+                style={{ display: "none" }}
+                onChange={handleImgProfileChange}
+              />
+              <img
+                className={styles.imgProfile}
+                src={currentImgProfile}
+                alt="imgProfile"
+              />
+            </label>
           </li>
           <li>{user.name}</li>
           <li>{user.lName}</li>
